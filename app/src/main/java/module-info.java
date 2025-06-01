a57940eec15b1503da0bb60c24c72fb35cc929d2
Bartek <bartoszsantander@gmail.com>
2025-06-01 11:22:02 +0200

    multi module initial commit

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module app {
+    requires spring.core;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+}
